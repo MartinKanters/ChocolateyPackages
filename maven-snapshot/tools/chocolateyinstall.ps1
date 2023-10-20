62f693f6ff4bbddba386429fbd366cada6afc2f2
Martin Kanters <martinkanters@noreply.github.com>
2023-10-20 16:13:32 +0000

    Update to latest Maven snapshot

--- maven-snapshot/tools/chocolateyinstall.ps1
+++ maven-snapshot/tools/chocolateyinstall.ps1
@@ -16,8 +16,8 @@ function IsM2HomeBinInPath ([string]$pathToAdd) {
 	}
 }
 
-$checksum = "216ea9668dd7e71fff9f29cba372f522284301d244999868bc679f2028fa30a3"
-$url = "https://repository.apache.org/content/groups/snapshots/org/apache/maven/apache-maven/4.0.0-alpha-8-SNAPSHOT/apache-maven-4.0.0-alpha-8-20231020.143113-95-bin.zip"
+$checksum = "c4a98334c4e67d40f1bf3b86c89778d673497f39bd7a16d169e85b5887a9f322"
+$url = "https://repository.apache.org/content/groups/snapshots/org/apache/maven/apache-maven/4.0.0-alpha-8-SNAPSHOT/apache-maven-4.0.0-alpha-8-20231020.152024-97-bin.zip"
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 $m2_home = Join-Path $package "apache-maven"
